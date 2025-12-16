aeb97f0d0f88ce5fdc4a6b01b8b5ac7497d971fd
darkarchorn <darkarchorn@gmail.com>
2025-12-16 15:28:17 +0700

    added response format for search agent

--- searchAgent.py
+++ searchAgent.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 
 load_dotenv()
+from typing import List
+from pydantic import BaseModel, Field
 from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
@@ -22,6 +24,15 @@ def search(query: str) -> str:
     print(f"Searching for {query}")
     return tavily.search(query)
 
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+    url: str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for agent response with answer and sources"""
+    answer:str = Field(default="", description="The agent's answer to the query")
+    sources:List[Source] = Field(default_factory=list, description= "The list of sources generate the answer")
+
 llm = ChatOpenAI(
     base_url="http://localhost:1234/v1",
     api_key="lm-studio",
@@ -29,12 +40,16 @@ llm = ChatOpenAI(
 )
 # tools = [search]
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 
 def main():
     print("Hello from langchain-course!")
-    result = agent.invoke({"messages": HumanMessage(content="what is the weather in Hanoi? give me the link to the source")})
+    result = agent.invoke(
+        {
+            "messages": HumanMessage(content="search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details?")
+        }
+    )
     print(result)
 
 main()
